1e033e15e675e8945eef5cb48f5b4b35fd47d086
Aman Fangeria <aman.fangeria@geekyants.com>
2024-08-27 13:10:57 +0530

    modified HelloWorld Animation

--- app/(animations)/HelloWorld.tsx
+++ app/(animations)/HelloWorld.tsx
@@ -1,29 +1,43 @@
 import { View, Animated, TouchableOpacity, Text } from "react-native";
 import React, { useState } from "react";
 const HelloWorld = () => {
-    const value = useState(new Animated.ValueXY({ x: 0, y: 0 }))[0];
+    // creating a animated value
+    const leftValue = useState(new Animated.Value(0))[0];
+
+    // setting the animated value
     const moveBall = () => {
-        Animated.timing(value, {
-            toValue: { x: 100, y: 100 },
+        Animated.timing(leftValue, {
+            toValue: 1000,
             duration: 1000,
             useNativeDriver: false,
         }).start();
     };
     return (
-        <View>
-            <Animated.View style={value.getLayout()}>
-                <View
-                    style={{
-                        width: 100,
-                        height: 100,
-                        borderRadius: 100 / 2,
-                        backgroundColor: "red",
-                    }}
-                ></View>
-            </Animated.View>
-            <TouchableOpacity onPress={moveBall}>
-                <Text>Click to move the ball</Text>
-            </TouchableOpacity>
+        <View style={{ flex: 1 }}>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Animated.View
+                    style={[
+                        {
+                            width: 100,
+                            height: 100,
+                            borderRadius: 100 / 2,
+                            backgroundColor: "red",
+                            // binding the animated value
+                            marginLeft: leftValue,
+                            marginBottom: 10,
+                        },
+                    ]}
+                ></Animated.View>
+                <TouchableOpacity onPress={moveBall}>
+                    <Text>Move the ball</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
